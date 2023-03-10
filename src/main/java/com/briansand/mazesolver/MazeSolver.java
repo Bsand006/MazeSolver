968a9d743eb222a4fc4f848c9c675cb3275d822b
brain <dr.skelington@protonmail.com>
2023-03-09 22:55:51 -0500

    Complete maybe?

--- src/main/java/com/briansand/mazesolver/MazeSolver.java
+++ src/main/java/com/briansand/mazesolver/MazeSolver.java
@@ -5,6 +5,14 @@ import java.io.FileNotFoundException;
 import java.util.LinkedList;
 import java.util.Scanner;
 
+/*
+ * MazeSolver.java
+ * This program takes in a maze stored in a file, and creates a corresponding grid of
+ * a position object. The program will then run a recursion method that moves through the cells
+ * until the maze is solved.
+ * @author Brian Sand
+ */
+
 public class MazeSolver {
 
 	Position[][] maze;
@@ -13,10 +21,11 @@ public class MazeSolver {
 
 	LinkedList<Position> stack = new LinkedList<>(); // Stack
 
-	/**
+	/*
 	 * The paramaters object stores the x and y coordinates of the start location,
 	 * and contains the dimensions of the maze for out of bounds checking.
 	 */
+	
 	static class Paramaters {
 		int x;
 		int y;
@@ -31,12 +40,16 @@ public class MazeSolver {
 		}
 	}
 
-	/**
+	/*
 	 * The position object stores the current coordinates that the game is on. Every
 	 * time the game moves to a new space a new object of this class will be
 	 * created, updated, and then pushed onto the stack to keep track of every move
 	 * that has been made.
+	 * 
+	 * The grid used is also a grid of this Position object. This allows me to store if a cell is
+	 * open or not, and to mark the path the program has taken so it can backtrack it needed.
 	 */
+	
 	static class Position {
 		int x;
 		int y;
@@ -77,17 +90,17 @@ public class MazeSolver {
 		}
 	}
 
-	/**
+	/*
 	 * mazeLoader creates the 2D array. The maze array is a maze of Position
 	 * objects. Creating the maze in this way allows me to store more information in
-	 * a cell. I can store what the that cell is, and more importantly I can store
+	 * a cell. I can store what that cell is, and more importantly I can store
 	 * information about the path that I have taken, which prevents the program for
 	 * constantly going back and forth and allows me to backtrack if required.
 	 * 
 	 * This data is stored using the state integer in the Position object. If it is
 	 * 0, then that cell is the starting cell. If it is 1, then it is an open path.
 	 * 2 is a wall and 3 is the end of the maze. 4 represents the "path", which are
-	 * the cells that I went to previously to get to where I am now.
+	 * the cells that the program took previously to get to where it is  now.
 	 */
 
 	public void mazeLoader() throws FileNotFoundException {
@@ -132,74 +145,88 @@ public class MazeSolver {
 		solveMaze(param.x, param.y);
 	}
 
-	/**
+	/*
 	 * solveMaze is the actual recursion solve method. It works by creating a new
 	 * Position object. The program then will check all directions until it finds a
-	 * valid move. I then sets the x and y coords of that point into the x and y
+	 * valid move. It then sets the x and y coords of that point into the x and y
 	 * integers in the method. The Position object is then pushed onto the stack and
 	 * calls itself, taking in the x and y integers as parameters. This is done
 	 * because a new blank Position object is created each time it runs, so the
 	 * current x and y coords need to be tracked seperately.
+	 * 
+	 * The program backtracks by setting the current cell to be closed and popping
+	 * the previous Position off the stack and setting x and y to that object. This
+	 * process repeats until the program can turn in a different direction.
 	 */
+
 	public boolean solveMaze(int x, int y) {
-		Position pos = new Position(); // Creates new Postion object
-	
+		Position pos = new Position(); // Creates new Position object
+
 		pos.setup(x, y, param.rows, param.cols); // Sets values of pos
 
-		// EAST
+		// Win condition check
+		if (x < pos.cols - 1 && x > 0 && y < pos.rows - 1 && y > 0) { // Out of bounds check
+			if (maze[y][x - 1].state == 3 || maze[y][x + 1].state == 3 || maze[y - 1][x].state == 3
+					|| maze[y + 1][x].state == 3) {
+				System.out.println("MAZE SOLVED!");
+				System.out.println("SOLVED IN " + turns + " TURNS");
+				
+				return true;
+			}
+		}
+
+		// Go east
 		if (x < pos.cols - 1 && maze[y][x + 1].state == 1) {
 			pos.EAST();
 			x = pos.x;
 			y = pos.y;
 			maze[y][x].state = 4;
-			System.out.println("MOVING EAST... COORDS ARE : " + x + " + " + y);
+			System.out.println("MOVING EAST... COORDS ARE : " + y + " + " + x);
 			turns++;
 			stack.push(pos);
 			solveMaze(x, y);
 
-			// WEST
+			// Go west
 		} else if (x > 0 && maze[y][x - 1].state == 1) {
 			pos.WEST();
 			x = pos.x;
 			y = pos.y;
 			maze[y][x].state = 4;
-			System.out.println("MOVING WEST... COORDS ARE : " + x + " + " + y);
+			System.out.println("MOVING WEST... COORDS ARE : " + y + " + " + x);
 			turns++;
 			stack.push(pos);
 			solveMaze(x, y);
 
-			// SOUTH
+			// Go south
 		} else if (y < pos.rows - 1 && maze[y + 1][x].state == 1) {
 			pos.SOUTH();
 			x = pos.x;
 			y = pos.y;
 			maze[y][x].state = 4;
-			System.out.println("MOVING SOUTH... COORDS ARE : " + x + " + " + y);
+			System.out.println("MOVING SOUTH... COORDS ARE : " + y + " + " + x);
 			turns++;
 			stack.push(pos);
 			solveMaze(x, y);
 
-			// NORTH
+			// Go north
 		} else if (y > 0 && maze[y - 1][x].state == 1) {
 			pos.NORTH();
 			x = pos.x;
 			y = pos.y;
 			maze[y][x].state = 4;
-			System.out.println("MOVING NORTH... COORDS ARE : " + x + " + " + y);
+			System.out.println("MOVING NORTH... COORDS ARE : " + y + " + " + x);
 			turns++;
 			stack.push(pos);
 			solveMaze(x, y);
 
-			// Win condition check
-		} else if (x < pos.cols - 1 && x > 0 && y < pos.rows - 1 && y > 0) {
-			if (maze[y][x - 1].state == 3 || maze[y][x + 1].state == 3 || maze[y - 1][x].state == 3
-					|| maze[y + 1][x].state == 3) {
-				System.out.println("MAZE SOLVED!");
-				System.out.println("SOLVED IN " + turns + " TURNS");
-				return true;
-			}
+			// If dead end reached
 		} else {
 			System.out.println("DEAD END! BACKTRACKING...");
+			maze[y][x].state = 2;
+			pos = stack.pop();
+			x = pos.x;
+			y = pos.y;
+			solveMaze(x, y);
 		}
 
 		return false;
